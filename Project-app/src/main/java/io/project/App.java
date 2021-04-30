b1267cd7e46cb222aabe66cc68cf6cc0e7fff4b7
hlebs <glebszypula1997@gmail.com>
2021-04-30 18:37:56 +0200

    dashboard fixes

--- Project-app/src/main/java/io/project/App.java
+++ Project-app/src/main/java/io/project/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("dashboard"));
         stage.setScene(scene);
         stage.show();
-        stage.setResizable(false);
+//        stage.setResizable(false);
     }
 
     static void setRoot(String fxml) throws IOException {
