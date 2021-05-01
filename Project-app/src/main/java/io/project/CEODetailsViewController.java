1081854ee7c8572c8e166d87b0ed5a9dbc9df719
atrria <ada.lesna@gmail.com>
2021-05-01 20:47:32 +0200

    Added CEO main page - UI and empty controller

---
+++ Project-app/src/main/java/io/project/CEODetailsViewController.java
@@ -0,0 +1,59 @@
+package io.project;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CEODetailsViewController implements Initializable {
+	
+	@FXML
+	private AnchorPane CEOPane;
+	
+	@FXML
+	private Label CEOFirstName;
+	
+	@FXML
+	private Label CEOLastName;
+	
+	@FXML
+	private Label CEOBirthDate;
+	
+	@FXML
+	private Label CEOPhoneNumber;
+	
+	@FXML
+	private Label CEOPosition;
+	
+	@FXML
+	private Label CEOCategory;
+	
+	@FXML
+	private Label CEOCompanyAccountBalance;
+	
+	@FXML
+	private Button CEOConfirmButton;
+	
+	@FXML
+	private Button CEOLogoutButton;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/io/project/CEODetailsView.fxml"));
+		try {
+			Parent root = loader.load();
+			CEOPane.getChildren().clear();
+			CEOPane.getChildren().addAll(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
