8b061f1b8f0466f243b979c1adbf586bfd15ad96
atrria <ada.lesna@gmail.com>
2021-05-01 17:38:22 +0200

    Added missing empty line

--- Project-app/src/main/java/io/project/EmployeeDetailsManagerViewController.java
+++ Project-app/src/main/java/io/project/EmployeeDetailsManagerViewController.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EmployeeDetailsManagerViewController implements Initializable {
+	
 	@FXML
 	private AnchorPane employeePane;
 	
