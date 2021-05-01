e592e8f65f3e0bab5997e11eaf86d8bf090852d9
atrria <ada.lesna@gmail.com>
2021-05-01 15:53:34 +0200

    Added employee main page ui and empty controller

---
+++ Project-app/src/main/java/io/project/EmployeeDetailsManagerViewController.java
@@ -0,0 +1,67 @@
+package io.project;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeDetailsManagerViewController implements Initializable {
+	@FXML
+	private AnchorPane employeePane;
+	
+	@FXML
+	private Label employeeFirstName;
+	
+	@FXML
+	private Label employeeLastName;
+	
+	@FXML
+	private Label employeeBirthDate;
+	
+	@FXML
+	private Label employeeEmploymentDate;
+	
+	@FXML
+	private Label employeePhoneNumber;
+	
+	@FXML
+	private Label employeePosition;
+	
+	@FXML
+	private Label employeeSalary;
+	
+	@FXML
+	private Button employeeDeleteButton;
+	
+	@FXML
+	private Button employeeAddCourseButton;
+	
+	@FXML
+	private Button employeeAssignToPlace;
+	
+	@FXML
+	private Label employeeFirstName1;
+	
+	@FXML
+	private ListView<?> employeeResponsibilitiesList;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/io/project/employeeDetailsManagerView.fxml"));
+		try {
+			Parent root = loader.load();
+			employeePane.getChildren().clear();
+			employeePane.getChildren().addAll(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
