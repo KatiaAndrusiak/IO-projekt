43bd7339bf5e925ae93b908ae4665e3c95d1c017
atrria <ada.lesna@gmail.com>
2021-05-03 23:33:06 +0200

    Added employee addition UI

---
+++ Project-app/src/main/java/io/project/EmployeeAdditionController.java
@@ -0,0 +1,43 @@
+package io.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeAdditionController implements Initializable
+{
+	@FXML
+	private AnchorPane employeeAdditionPane;
+	
+	@FXML
+	private TextField employeeFirstNameTF;
+	
+	@FXML
+	private TextField employeeLastNameTF;
+	
+	@FXML
+	private DatePicker employeeDOBTF;
+	
+	@FXML
+	private TextField employeePositionTF;
+	
+	@FXML
+	private TextField employeeSalaryTF;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/io/project/employeeAddition.fxml"));
+		try {
+			Parent root = loader.load();
+			employeeAdditionPane.getChildren().clear();
+			employeeAdditionPane.getChildren().addAll(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
