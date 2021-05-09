bfab7c79d302e0e039b5cd2ab79c0e6610eea0d0
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2021-05-10 00:05:37 +0200

    add holiday

--- Project-app/src/main/java/io/project/HolidayAdditionController.java
+++ Project-app/src/main/java/io/project/HolidayAdditionController.java
@@ -1,15 +1,18 @@
 package io.project;
 
+import io.project.alert.AlertBox;
+import io.project.database.DBManagment;
+import io.project.entities.Employee;
+import io.project.entities.Holiday;
+import io.project.validation.CheckAndClearTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,7 +22,7 @@ public class HolidayAdditionController implements Initializable
 	private AnchorPane holidayAdditionPane;
 	
 	@FXML
-	private TextField holidayNameTF;
+	private ComboBox<String> holidayNameComboBox;
 	
 	@FXML
 	private DatePicker holidayDateTF;
@@ -28,17 +31,35 @@ public class HolidayAdditionController implements Initializable
 	private TextField holidayIncomeTF;
 	
 	@FXML
-	private ComboBox<?> holidayPersonComboBox;
+	private ComboBox<Employee> holidayPersonComboBox;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/io/project/holidayAddition.fxml"));
+		/*FXMLLoader loader = new FXMLLoader(getClass().getResource("/io/project/holidayAddition.fxml"));
 		try {
 			Parent root = loader.load();
 			holidayAdditionPane.getChildren().clear();
 			holidayAdditionPane.getChildren().addAll(root);
+			DBManagment.fillHolidayAdditionData(holidayPersonComboBox);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}*/
+		DBManagment.fillHolidayAdditionDataEmployee(holidayPersonComboBox);
+		DBManagment.fillHolidayAdditionDataHoliday(holidayNameComboBox);
 	}
+
+    public void addHoliday(ActionEvent actionEvent) {
+		Holiday holidayToAdd = new Holiday();
+		holidayToAdd.setName(holidayNameComboBox.getSelectionModel().getSelectedItem());
+		holidayToAdd.setProceeds(Double.parseDouble(holidayIncomeTF.getText()));
+		holidayToAdd.setEmployee(holidayPersonComboBox.getSelectionModel().getSelectedItem());
+		holidayToAdd.setDate(holidayDateTF.getValue());
+		holidayToAdd.setFacility(Global.getFacility());
+		if (DBManagment.addHoliday(holidayToAdd)) {
+			AlertBox.infoAlert("Udało się!", "Dodano święto " + holidayToAdd.getName() + ", do pracownika " + holidayToAdd.getEmployee().getFirstName() + ", " + holidayToAdd.getEmployee().getFirstName() , "Obiekt został dodany do bazy");
+			CheckAndClearTextField.clearTextField(holidayIncomeTF);
+		}else{
+			AlertBox.infoAlert("Ups", "Nie udało się dodać święta", "Obiekt nie został dodany");
+		}
+    }
 }
