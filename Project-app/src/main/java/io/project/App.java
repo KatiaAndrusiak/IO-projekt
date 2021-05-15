bc7c9a84efdcb10a065091aa24284219fc41bebb
Vadym <vsamkovich@ukr.net>
2021-05-15 15:14:07 +0200

    search employee functionality in employeeList was added

--- Project-app/src/main/java/io/project/App.java
+++ Project-app/src/main/java/io/project/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("supplierAddition"));
+        scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
         stage.show();
         stage.setTitle("Panel logowania");
