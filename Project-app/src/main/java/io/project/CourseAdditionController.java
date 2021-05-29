2a16aae83d9f0e6758914a987bb035e808ff615c
Vadym <vsamkovich@ukr.net>
2021-05-29 15:16:27 +0200

    fix small problem in CourseAddition

--- Project-app/src/main/java/io/project/CourseAdditionController.java
+++ Project-app/src/main/java/io/project/CourseAdditionController.java
@@ -4,9 +4,11 @@ import io.project.alert.AlertBox;
 import io.project.database.DBManagment;
 import io.project.entities.Course;
 import io.project.entities.Employee;
+import io.project.screenloader.ChangeScreen;
 import io.project.validation.FieldValidation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
@@ -31,11 +33,13 @@ public class CourseAdditionController implements Initializable
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		FieldValidation.filterForNumberFields(courseHoursTF);
+		FieldValidation.filterForTextFields(courseNameTF);
 	}
 	public void addCourseToEmployee(ActionEvent event) {
 		try {
 			if(FieldValidation.validateCharField(courseNameTF)
-					&& FieldValidation.validateNum(courseHoursTF)
+					&& FieldValidation.validateCoursesHours(courseHoursTF)
 			&& FieldValidation.validateDatePicker(courseDateTF)) {
 				Course courseToAdd = new Course();
 				Employee employee = Global.getEmployee();
@@ -44,10 +48,11 @@ public class CourseAdditionController implements Initializable
 				courseToAdd.setEmployee(employee);
 				courseToAdd.setDate(courseDateTF.getValue());
 				if (DBManagment.addCourseToEmployee(courseToAdd)) {
-					AlertBox.infoAlert("Udało się!", "Dodano kurs " + courseToAdd.getName() + ", do pracownika " + courseToAdd.getEmployee().getFirstName() + ", " + courseToAdd.getEmployee().getFirstName(), "Obiekt został dodany do bazy");
+					AlertBox.infoAlert("Udało się!", "Dodano kurs " + courseToAdd.getName() + ", do pracownika " + courseToAdd.getEmployee().getFirstName() + ", " + courseToAdd.getEmployee().getLastName(), "Obiekt został dodany do bazy");
 					employee.setCourseHoursSum(employee.getCourseHoursSum() + courseToAdd.getHours());
 					courseNameTF.clear();
 					courseHoursTF.clear();
+					ChangeScreen.initPanel(Global.getViewPane(), FXMLLoader.load(getClass().getResource("employeeDetailsManagerView.fxml")));
 				} else {
 					AlertBox.errorAlert("Błąd", "Nie udalo sie dodac");
 				}
